9a527ee5a5b4ab19a7ca1aea353e283a85571d68
Jessica Stewart <jeselistewart@gmail.com>
2015-11-15 11:22:58 -0800

    Twos complement exercise

--- bits.s
+++ bits.s
@@ -1,8 +1,24 @@
+.segment __DATA,__bss
+  sum resb 1
+
+.segment __TEXT,__text
 .globl start
 start:
         movl $0x2F,  %r8d
         movl $0x34,  %r9d
         add  %r8d, %r9d
         movl %r9d, %edi
+
+        movl [sum], eax
+        movl ebx, 1
+        movl eax, 4
+        syscall
+
+        movl ecx, sum
+        movl $1, %edx
+        movl $1, %ebx
+        mov eax, 4
+        syscall
+
         movl $0x2000001, %eax
         syscall
