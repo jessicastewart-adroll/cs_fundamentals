4af4467fd23b46f23964f3a6cdac2c24322ea65b
Jessica Stewart <jessica.stewart@adroll.com>
2015-11-30 10:03:09 -0800

    Solution for exercise 1

--- iv_networking_socket/socket-hw.py
+++ iv_networking_socket/socket-hw.py
@@ -2,8 +2,9 @@
 I.
     Use 17.2 socket to play with the program
     Make accept more than one connection successively
-    Terminate connection on ctrl+D or 'quit' input
-    ??? program hangs
+    Terminate connection on ctrl+C or 'quit' input
+
+client quit > server keyboard interrupt > create new binding on server: [Errno 48] Address already in use
 
 II.
     openssl command -> connect to Google over https
@@ -16,21 +17,49 @@ III.
         Hfr gur -pbaarpg bcgvba. -> Use the -connect option
 '''
 import socket
+import sys
+from thread import start_new_thread
 
 TCP_IP = '0.0.0.0'  # This means that we should bind to all the addresses your computer has
 TCP_PORT = 5005  # Port we're listening on
-BUFFER_SIZE = 20  # In bytes, how many bytes to wait for before recv returns
+BUFFER_SIZE = 1024  # In bytes, how many bytes to wait for before recv returns
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # It's an Internet socket, and it's a stream
-s.bind((TCP_IP, TCP_PORT))  # bind the socket to the IP + port
-s.listen(1)  # start listening on that socket
-conn, addr = s.accept()  # Accept a connection from the socket
+def connection_thread(conn):
+    conn.send("Client socket: Welcome! Type something and hit enter\n")
+    while True:
+        data = conn.recv(BUFFER_SIZE)
+        if not data:
+            break
 
-print 'Connection address:', addr
-while 1:
-    data = conn.recv(BUFFER_SIZE)
-    if not data:
-        break
-    print "received data:", data
-    conn.send(data)
-conn.close()
+        if data == 'quit\r\n':
+            conn.send("Client socket: closing socket.")
+            conn.close()
+            return
+        else:
+            reply = "Client socket data: " + data
+            conn.sendall(reply)
+
+try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # It's an Internet socket, and it's a stream
+    print "Server socket created."
+    s.bind((TCP_IP, TCP_PORT))  # bind the socket to the IP + port
+    print "Server socket bind complete."
+    s.listen(5)  # start listening on that socket
+    print "Server socket listening for up to 5 connections."
+    conn = None
+
+    while 1:
+        conn, addr = s.accept()  # Accept a connection from the socket
+        print "Server socket connected with IP:" + addr[0] + ", port:" + str(addr[1])
+        start_new_thread(connection_thread, (conn,))
+
+except KeyboardInterrupt:
+    print "Closing server socket and connection through keyboard interrupt"
+    if conn:
+        conn.close()
+    if s:
+        s.close()
+
+except Exception as e:
+    print "Exception thrown,{}".format(e)
+    sys.exit(1)
