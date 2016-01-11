d3fb28ba8200c5b67595fc8f8c44e3e213497ac7
Jess Stewart <jessica.stewart@adroll.com>
2016-01-11 14:36:26 -0800

    Adds Paul's homework assignment

---
+++ v_networking_socket/socket-hw.py
@@ -0,0 +1,18 @@
+import socket
+TCP_IP = '0.0.0.0'  # This means that we should bind to all the addresses your computer has
+TCP_PORT = 5005  # Port we're listening on
+BUFFER_SIZE = 20  # In bytes, how many bytes to wait for before recv returns
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # It's an Internet socket, and it's a stream
+s.bind((TCP_IP, TCP_PORT))  # bind the socket to the IP + port
+s.listen(2)  # start listening on that socket, let 2 clients wait in the queue before refusing connections
+while 1:
+    conn, addr = s.accept()  # Accept a connection from the socket
+    while 1:
+        data = conn.recv(BUFFER_SIZE) # wait for data of 20 bytes or more.
+        if not data:
+            break
+        if "quit" in data:
+            break
+        print "received data: ", data
+        conn.send(data)
+    conn.close()
