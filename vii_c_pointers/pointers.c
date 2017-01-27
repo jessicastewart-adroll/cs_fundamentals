3c62a25e61f07673ab3f713e1bd78aa28fc3f10b
Jess Stewart <jeselistewart@gmail.com>
2017-01-27 15:34:23 -0800

    Update pointers.c

--- vii_c_pointers/pointers.c
+++ vii_c_pointers/pointers.c
@@ -3,15 +3,36 @@
 /* gcc -o pointers pointers.c */
 /* ./pointers */
 
-int *add_five(int *num) {
-	*num = *num + 5;
-	return num;
+void add(int *num1, int num2) {
+	*num1 = *num1 + num2;
+}
+
+void subtract(int *num1, int num2) {
+	*num1 = *num1 - num2;
+}
+
+void multiply(int *num1, int num2) {
+	*num1 = *num1 * num2;
+}
+
+void divide(int *num1, int num2) {
+	*num1 = *num1 / num2;
 }
  
 int main()
 {
 	int x = 0;
-    add_five(&x);
-    printf("hello world %d", x);
+
+    add(&x, 6);
+    printf("Integer after addition %d\n", x);
+
+    subtract(&x, 1);
+    printf("Integer after subtraction %d\n", x);
+        
+    multiply(&x, 2);
+    printf("Integer after multiplication %d\n", x);
+        
+    divide(&x, 5);
+    printf("Integer after division %d\n", x);
     return 0;
 }
