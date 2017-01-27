c979efc02724478cf85060a92c0286a22af56ec9
Jess Stewart <jeselistewart@gmail.com>
2017-01-27 13:11:23 -0800

    Update pointers.c

--- vii_c_pointers/pointers.c
+++ vii_c_pointers/pointers.c
@@ -2,9 +2,16 @@
 
 /* gcc -o pointers pointers.c */
 /* ./pointers */
+
+int *add_five(int *num) {
+	*num = *num + 5;
+	return num;
+}
  
 int main()
 {
-    printf("Hello world\n");
+	int x = 0;
+    add_five(&x);
+    printf("hello world %d", x);
     return 0;
 }
